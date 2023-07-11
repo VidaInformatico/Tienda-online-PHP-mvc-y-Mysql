701c2fe73ad1f5086cf6fc20bf705ee1bf75589e
Angel Sifuentes <71534078+VidaInformatico@users.noreply.github.com>
2023-07-11 15:53:53 -0500

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -7,7 +7,7 @@ const DB = "tienda-youtube";
 const CHARSET = "charset=utf8";
 const TITLE = "TIENDA ONLINE";
 const MONEDA = "USD";
-const CLIENT_ID = "ARtELM9gW5sQcYEDb-11-jdoPAig6Y--f9Ggu9-RStP0QO4QrECMBHzzQaownQd1lZCL4yoJEOmYo8CN";
+const CLIENT_ID = "";
 
 const USER_SMTP = "Tu correo";
 const PASS_SMTP = "contraseña generada para esta aplicacion";
